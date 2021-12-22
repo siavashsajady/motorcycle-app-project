1bff9e9eea7c8336af22b428b74e23898d8561e2
siavashsajady <hubgitpro@gmail.com>
2021-12-22 10:18:59 +0100

    install react-burger-menu/create burger menu to display in mobile devices/  Create Responsive Navbar component

--- src/app/components/Navbar/NavItems.tsx
+++ src/app/components/Navbar/NavItems.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import tw from 'twin.macro';
+import { slide as Menu } from 'react-burger-menu';
+import { useMediaQuery } from 'react-responsive';
+import { SCREENS } from '../Responsive/Responsive';
+import MenuStyles from './MenuStyles';
 
 const ItemsContainer = styled.ul`
   ${tw`
@@ -9,7 +13,7 @@ const ItemsContainer = styled.ul`
 
     `}
 `;
-const NavItem = styled.li`
+const NavItem = styled.li<{ menu?: any }>`
   ${tw`
         text-xs
         md:text-base 
@@ -23,9 +27,40 @@ const NavItem = styled.li`
         ease-in-out 
         hover:text-gray-700 
     `}
+  ${({ menu }) =>
+    menu &&
+    css`
+      ${tw`
+            text-white
+            text-xl 
+            mb-3 
+            focus:text-white
+        `}
+    `}
 `;
 
 export function NavItems() {
+  const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
+
+  if (isMobile)
+    return (
+      <Menu right styles={MenuStyles}>
+        <ItemsContainer>
+          <NavItem menu>
+            <a href='#'>Home</a>
+          </NavItem>
+          <NavItem menu>
+            <a href='#'>MotorCycles</a>
+          </NavItem>
+          <NavItem menu>
+            <a href='#'>Services</a>
+          </NavItem>
+          <NavItem menu>
+            <a href='#'>Contact Us</a>
+          </NavItem>
+        </ItemsContainer>
+      </Menu>
+    );
   return (
     <ItemsContainer>
       <NavItem>
